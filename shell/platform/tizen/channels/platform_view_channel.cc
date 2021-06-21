58d78e0d33f40d1c3b2dc235fc7c59c5a1d1ee28
MuHong Byun <mh.byun@samsung.com>
2021-06-22 07:40:57 +0900

    Refactor platform view channel which is related to clean up view resources
    Signed-off-by: MuHong Byun <mh.byun@samsung.com>

--- shell/platform/tizen/channels/platform_view_channel.cc
+++ shell/platform/tizen/channels/platform_view_channel.cc
@@ -49,12 +49,30 @@ PlatformViewChannel::~PlatformViewChannel() {
 }
 
 void PlatformViewChannel::Dispose() {
+  ClearViewInstances();
+  ClearViewFactories();
+}
+
+void PlatformViewChannel::RemoveViewInstanceIfNeeded(int view_id) {
+  auto it = view_instances_.find(view_id);
+  if (view_id >= 0 && it != view_instances_.end()) {
+    auto view_instance = it->second;
+    view_instance->Dispose();
+    delete view_instance;
+    view_instances_.erase(it);
+  }
+}
+
+void PlatformViewChannel::ClearViewInstances() {
   // Clean-up view_instances_
   for (auto const& [view_id, view_instance] : view_instances_) {
+    view_instance->Dispose();
     delete view_instance;
   }
   view_instances_.clear();
+}
 
+void PlatformViewChannel::ClearViewFactories() {
   // Clean-up view_factories_
   for (auto const& [view_type, view_factory] : view_factories_) {
     view_factory->Dispose();
@@ -121,6 +139,7 @@ void PlatformViewChannel::HandleMethodCall(
     FT_LOGI(
         "PlatformViewChannel create viewType: %s id: %d width: %f height: %f ",
         view_type.c_str(), view_id, width, height);
+    RemoveViewInstanceIfNeeded(view_id);
 
     flutter::EncodableMap values = std::get<flutter::EncodableMap>(arguments);
     flutter::EncodableValue value = values[flutter::EncodableValue("params")];
@@ -133,12 +152,6 @@ void PlatformViewChannel::HandleMethodCall(
       auto focused_view = view_instances_.find(CurrentFocusedViewId());
       if (focused_view != view_instances_.end()) {
         focused_view->second->SetFocus(false);
-      }
-
-      auto existing_view = view_instances_.find(view_id);
-      if (existing_view != view_instances_.end()) {
-        existing_view->second->Dispose();
-        view_instances_.erase(existing_view);
       }
 
       auto view_instance =
@@ -173,6 +186,17 @@ void PlatformViewChannel::HandleMethodCall(
     } else {
       result->Error("Can't find view id");
     }
+  } else if (method == "dispose") {
+    int view_id = -1;
+    if (std::holds_alternative<int>(arguments)) {
+      view_id = std::get<int>(arguments);
+    };
+    if (view_id < 0 || view_instances_.find(view_id) == view_instances_.end()) {
+      result->Error("Can't find view id");
+    } else {
+      RemoveViewInstanceIfNeeded(view_id);
+      result->Success();
+    }
   } else {
     int view_id = -1;
     if (!GetValueFromEncodableMap(arguments, "id", &view_id)) {
@@ -182,10 +206,7 @@ void PlatformViewChannel::HandleMethodCall(
 
     auto it = view_instances_.find(view_id);
     if (view_id >= 0 && it != view_instances_.end()) {
-      if (method == "dispose") {
-        it->second->Dispose();
-        result->Success();
-      } else if (method == "resize") {
+      if (method == "resize") {
         double width = 0.0, height = 0.0;
         if (!GetValueFromEncodableMap(arguments, "width", &width) ||
             !GetValueFromEncodableMap(arguments, "height", &height)) {
