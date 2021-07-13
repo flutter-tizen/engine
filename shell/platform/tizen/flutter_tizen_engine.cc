7f5127dbacb68460ecfd55667f4ec24fb65005a3
Swift Kim <swift.kim@samsung.com>
2021-07-13 20:43:27 +0900

    Rename tizen_log to logger

--- shell/platform/tizen/flutter_tizen_engine.cc
+++ shell/platform/tizen/flutter_tizen_engine.cc
@@ -9,8 +9,8 @@
 #include <string>
 #include <vector>
 
+#include "flutter/shell/platform/tizen/logger.h"
 #include "flutter/shell/platform/tizen/system_utils.h"
-#include "flutter/shell/platform/tizen/tizen_log.h"
 
 namespace flutter {
 
