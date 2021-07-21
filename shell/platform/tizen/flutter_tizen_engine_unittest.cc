b92e76f23526c237632c8c7ab8568d29145ffddf
Swift Kim <swift.kim@samsung.com>
2021-07-21 15:30:27 +0900

    Disable headed tests

--- shell/platform/tizen/flutter_tizen_engine_unittest.cc
+++ shell/platform/tizen/flutter_tizen_engine_unittest.cc
@@ -40,14 +40,6 @@ class FlutterTizenEngineTest : public ::testing::Test {
   FlutterTizenEngine* engine_;
 };
 
-class FlutterTizenEngineTestHeaded : public FlutterTizenEngineTest {
- protected:
-  void SetUp() {
-    FlutterTizenEngineTest::SetUp();
-    engine_->InitializeRenderer(0, 0, 800, 600, false, true);
-  }
-};
-
 TEST_F(FlutterTizenEngineTest, Run) {
   EXPECT_TRUE(engine_ != nullptr);
   EXPECT_TRUE(engine_->RunEngine(nullptr));
@@ -77,11 +69,6 @@ TEST_F(FlutterTizenEngineTest, GetPluginRegistrar) {
 TEST_F(FlutterTizenEngineTest, GetTextureRegistrar) {
   EXPECT_TRUE(engine_->RunEngine(nullptr));
   EXPECT_TRUE(engine_->GetTextureRegistrar() == nullptr);
-}
-
-TEST_F(FlutterTizenEngineTestHeaded, GetTextureRegistrar) {
-  EXPECT_TRUE(engine_->RunEngine(nullptr));
-  EXPECT_TRUE(engine_->GetTextureRegistrar() != nullptr);
 }
 
 TEST_F(FlutterTizenEngineTest, RunDoesExpectedInitialization) {
