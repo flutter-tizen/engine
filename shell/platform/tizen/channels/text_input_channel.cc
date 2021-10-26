a47447188d3ed60e253d4554224bc0b33bdef10f
MuHong Byun <mh.byun@samsung.com>
2021-10-26 14:44:08 +0900

    Apply review's comment
    Signed-off-by: MuHong Byun <mh.byun@samsung.com>

--- shell/platform/tizen/channels/text_input_channel.cc
+++ shell/platform/tizen/channels/text_input_channel.cc
@@ -247,7 +247,7 @@ void TextInputChannel::HandleMethodCall(
                                       ? composing_extent->value.GetInt()
                                       : -1;
 
-    if (composing_base_value < 0 || composing_extent_value < 0) {
+    if (composing_base_value == -1 && composing_extent_value == -1) {
       active_model_->EndComposing();
     } else {
       size_t composing_start =
@@ -255,7 +255,8 @@ void TextInputChannel::HandleMethodCall(
       size_t cursor_offset = selection_base_value - composing_start;
 
       active_model_->SetComposingRange(
-          flutter::TextRange(composing_base_value, composing_extent_value),
+          flutter::TextRange(static_cast<size_t>(composing_base_value),
+                             static_cast<size_t>(composing_extent_value)),
           cursor_offset);
     }
     SendStateUpdate(*active_model_);
