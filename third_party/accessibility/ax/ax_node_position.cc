6a9c046004d87aeef48c20368580c9148ffbfaa2
wanchao.xu <wanchao.xu@samsung.com>
2022-03-07 16:03:35 +0800

    Remove the unnecessary code.

--- third_party/accessibility/ax/ax_node_position.cc
+++ third_party/accessibility/ax/ax_node_position.cc
@@ -14,7 +14,7 @@
 namespace ui {
 
 AXEmbeddedObjectBehavior g_ax_embedded_object_behavior =
-#if defined(OS_WIN) || defined(OS_LINUX)
+#if defined(OS_WIN)
     AXEmbeddedObjectBehavior::kExposeCharacter;
 #else
     AXEmbeddedObjectBehavior::kSuppressCharacter;
