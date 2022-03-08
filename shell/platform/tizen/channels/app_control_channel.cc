065f1fabc84a1babbcd27dd1dac55aae056d6e41
Swift Kim <swift.kim@samsung.com>
2022-03-08 14:11:43 +0900

    Remove support for deprecated methods

--- shell/platform/tizen/channels/app_control_channel.cc
+++ shell/platform/tizen/channels/app_control_channel.cc
@@ -65,20 +65,6 @@ void AppControlChannel::HandleMethodCall(
     std::unique_ptr<MethodResult<EncodableValue>> result) {
   const auto& method_name = method_call.method_name();
 
-  // The methods "create" and "dispose" are deprecated and will be removed in
-  // the future.
-  if (method_name == "create") {
-    auto app_control = std::make_unique<AppControl>();
-    if (app_control->handle()) {
-      result->Success(EncodableValue(app_control->id()));
-      AppControlManager::GetInstance().Insert(std::move(app_control));
-    } else {
-      result->Error("Internal error",
-                    "Could not create an instance of AppControl.");
-    }
-    return;
-  }
-
   auto* arguments = std::get_if<EncodableMap>(method_call.arguments());
   if (!arguments) {
     result->Error("Invalid arguments");
@@ -96,10 +82,7 @@ void AppControlChannel::HandleMethodCall(
     return;
   }
 
-  if (method_name == "dispose") {
-    AppControlManager::GetInstance().Remove(app_control->id());
-    result->Success();
-  } else if (method_name == "getMatchedAppIds") {
+  if (method_name == "getMatchedAppIds") {
     EncodableList app_ids;
     AppControlResult ret = app_control->GetMatchedAppIds(app_ids);
     if (ret) {
@@ -146,38 +129,18 @@ void AppControlChannel::Reply(
   }
 
   EncodableValueHolder<int32_t> reply_id(arguments, "replyId");
-  if (reply_id) {
-    auto* reply_app_control =
-        AppControlManager::GetInstance().FindById(*reply_id);
-    if (!reply_app_control) {
-      result->Error("Invalid arguments",
-                    "No instance of AppControl matches the given ID.");
-      return;
-    }
-    AppControlResult ret = app_control->Reply(reply_app_control, *result_str);
-    if (ret) {
-      result->Success();
-    } else {
-      result->Error(ret.code(), ret.message());
-    }
+  if (!reply_id) {
+    result->Error("Invalid arguments", "No replyId provided.");
     return;
   }
-
-  // Deprecated. Use replyId instead.
-  EncodableValueHolder<int32_t> request_id(arguments, "requestId");
-  if (!request_id) {
-    result->Error("Invalid arguments",
-                  "Either replyId or requestId must be provided.");
-    return;
-  }
-  auto* request_app_control =
-      AppControlManager::GetInstance().FindById(*request_id);
-  if (!request_app_control) {
+  auto* reply_app_control =
+      AppControlManager::GetInstance().FindById(*reply_id);
+  if (!reply_app_control) {
     result->Error("Invalid arguments",
                   "No instance of AppControl matches the given ID.");
     return;
   }
-  AppControlResult ret = request_app_control->Reply(app_control, *result_str);
+  AppControlResult ret = app_control->Reply(reply_app_control, *result_str);
   if (ret) {
     result->Success();
   } else {
