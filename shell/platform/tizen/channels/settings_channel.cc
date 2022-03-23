e2c59e22e7ae58f7015dfd53012b95b8a77df607
wanchao.xu <wanchao.xu@samsung.com>
2022-03-23 09:49:53 +0800

    Implement the large font for accessibility.

--- shell/platform/tizen/channels/settings_channel.cc
+++ shell/platform/tizen/channels/settings_channel.cc
@@ -32,18 +32,26 @@ SettingsChannel::SettingsChannel(BinaryMessenger* messenger)
         self->SendSettingsEvent();
       },
       this);
+  system_settings_set_changed_cb(
+      SYSTEM_SETTINGS_KEY_FONT_SIZE,
+      [](system_settings_key_e key, void* user_data) -> void {
+        auto* self = reinterpret_cast<SettingsChannel*>(user_data);
+        self->SendSettingsEvent();
+      },
+      this);
   SendSettingsEvent();
 }
 
 SettingsChannel::~SettingsChannel() {
   system_settings_unset_changed_cb(
       SYSTEM_SETTINGS_KEY_LOCALE_TIMEFORMAT_24HOUR);
+  system_settings_unset_changed_cb(SYSTEM_SETTINGS_KEY_FONT_SIZE);
 }
 
 void SettingsChannel::SendSettingsEvent() {
   rapidjson::Document event(rapidjson::kObjectType);
   auto& allocator = event.GetAllocator();
-  event.AddMember(kTextScaleFactorKey, 1.0, allocator);
+  event.AddMember(kTextScaleFactorKey, GetTextScaleFactor(), allocator);
   event.AddMember(kAlwaysUse24HourFormatKey, Prefer24HourTime(), allocator);
   event.AddMember(kPlatformBrightnessKey, "light", allocator);
   channel_->Send(event);
@@ -57,6 +65,38 @@ bool SettingsChannel::Prefer24HourTime() {
     return value;
   }
   return false;
+}
+
+float SettingsChannel::GetTextScaleFactor() {
+  const float small = 0.8;
+  const float normal = 1.0;
+  const float large = 1.5;
+  const float huge = 1.9;
+  const float giant = 2.5;
+
+  int value = 1;
+  if (system_settings_get_value_int(SYSTEM_SETTINGS_KEY_FONT_SIZE, &value) ==
+      SYSTEM_SETTINGS_ERROR_NONE) {
+    float scale = normal;
+    switch (value) {
+      case SYSTEM_SETTINGS_FONT_SIZE_SMALL:
+        scale = small;
+        break;
+      case SYSTEM_SETTINGS_FONT_SIZE_LARGE:
+        scale = large;
+        break;
+      case SYSTEM_SETTINGS_FONT_SIZE_HUGE:
+        scale = huge;
+        break;
+      case SYSTEM_SETTINGS_FONT_SIZE_GIANT:
+        scale = giant;
+        break;
+      default:
+        break;
+    }
+    return scale;
+  }
+  return 1.0;
 }
 
 }  // namespace flutter
