25e90327084119a59647bf2e839b8108b0fc5d5f
Swift Kim <swift.kim@samsung.com>
2022-06-10 10:41:31 +0900

    Move channel key maps to key_mapping.h

--- shell/platform/tizen/channels/key_mapping.h
+++ shell/platform/tizen/channels/key_mapping.h
@@ -5,15 +5,37 @@
 #ifndef EMBEDDER_KEY_MAPPING_H_
 #define EMBEDDER_KEY_MAPPING_H_
 
-#include <cinttypes>
 #include <map>
 #include <string>
 
-// Maps XKB specific key code values to Flutter's physical key code values.
-extern std::map<uint64_t, uint64_t> kXkbToPhysicalKeyCode;
+// Maps special TV key symbols to standard XKB scan codes.
+//
+// The values are originally defined in:
+// - xkb-tizen-data/tizen_key_layout.txt.tv
+// - flutter/keyboard_maps.dart (kLinuxToPhysicalKey)
+extern std::map<std::string, uint32_t> kSymbolToScanCode;
 
-// Maps Tizen key symbols to Flutter's logical key code values.
-extern std::map<std::string, uint64_t> kKeySymbolToLogicalKeyCode;
+// Maps XKB scan codes to GTK key codes.
+//
+// The values are originally defined in:
+// - flutter/keyboard_maps.dart (kLinuxToPhysicalKey)
+// - flutter/keyboard_maps.dart (kGtkToLogicalKey)
+extern std::map<uint32_t, uint32_t> kScanCodeToGtkKeyCode;
+
+// Mapping from Ecore modifiers to GTK modifiers.
+//
+// The values are originally defined in:
+// - efl/Ecore_Input.h
+// - flutter/raw_keyboard_linux.dart (GtkKeyHelper)
+extern std::map<int, int> kEcoreModifierToGtkModifier;
+
+// Maps XKB scan codes to Flutter's physical key codes.
+//
+// This is a copy of the Linux embedder's |xkb_to_physical_key_map|.
+extern std::map<uint64_t, uint64_t> kScanCodeToPhysicalKeyCode;
+
+// Maps Tizen key symbols to Flutter's logical key codes.
+extern std::map<std::string, uint64_t> kSymbolToLogicalKeyCode;
 
 // Mask for the 32-bit value portion of the key code.
 const uint64_t kValueMask = 0x000ffffffff;
