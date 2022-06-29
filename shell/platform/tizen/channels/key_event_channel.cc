7803f8fd3f3f0dc543523ac7becc57ef1fc5add6
Boram Bae <boram21.bae@samsung.com>
2022-06-29 16:31:10 +0900

    Remove navigation key handling
    * Navigation key event should be handled by the flutter framework.
    * Update related comments.
    * Fix incorrect type usage.
    
    Signed-off-by: Boram Bae <boram21.bae@samsung.com>

--- shell/platform/tizen/channels/key_event_channel.cc
+++ shell/platform/tizen/channels/key_event_channel.cc
@@ -62,6 +62,27 @@ uint64_t GetLogicalKey(const char* key) {
   return ApplyPlaneToId(0, kTizenPlane);
 }
 
+uint32_t GetFallbackScanCodeFromKey(std::string key) {
+  // Some of the scan codes are 0 when keys event occur from the software
+  // keyboard. then the key_event_channel cannot handle the key event.
+  // to avoid this, use a valid scan codes.
+  int scan_code = 0;
+
+  // The following keys can be emitted from the software keyboard and have a
+  // scan code with 0 value.
+  const std::map<std::string, uint32_t> kKeyToScanCode = {
+      {"BackSpace", 0x00000016}, {"Up", 0x0000006f},   {"Left", 0x00000071},
+      {"Right", 0x00000072},     {"Down", 0x00000074},
+  };
+
+  auto iter1 = kKeyToScanCode.find(key);
+  if (iter1 != kKeyToScanCode.end()) {
+    scan_code = iter1->second;
+  }
+
+  return scan_code;
+}
+
 }  // namespace
 
 KeyEventChannel::KeyEventChannel(BinaryMessenger* messenger,
@@ -98,6 +119,10 @@ void KeyEventChannel::SendKey(const char* key,
         << "framework. Are responses being sent?";
   }
   pending_events_[sequence_id] = std::make_unique<PendingEvent>(pending);
+
+  if (scan_code == 0) {
+    scan_code = GetFallbackScanCodeFromKey(key);
+  }
 
   SendEmbedderEvent(key, string, compose, modifiers, scan_code, is_down,
                     sequence_id);
