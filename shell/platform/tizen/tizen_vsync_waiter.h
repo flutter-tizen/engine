ef8fd0d4e667fb5a72b4efb9a089545e5a02b76a
Swift Kim <swift.kim@samsung.com>
2022-09-22 17:25:15 +0900

    Revert "Remove extra threading from TizenVsyncWaiter"
    This reverts commit 885e8bc7e202ccc53d2d48b03e2286237a0dbde2.

--- shell/platform/tizen/tizen_vsync_waiter.h
+++ shell/platform/tizen/tizen_vsync_waiter.h
@@ -5,21 +5,25 @@
 #ifndef EMBEDDER_TIZEN_VSYNC_WAITER_H_
 #define EMBEDDER_TIZEN_VSYNC_WAITER_H_
 
+#include <Ecore.h>
 #include <tdm_client.h>
 
 #include <memory>
 #include <mutex>
 
+#include "flutter/shell/platform/embedder/embedder.h"
+
 namespace flutter {
 
 class FlutterTizenEngine;
 
-class TizenVsyncWaiter {
+class TdmClient {
  public:
-  TizenVsyncWaiter(FlutterTizenEngine* engine);
-  virtual ~TizenVsyncWaiter();
+  TdmClient(FlutterTizenEngine* engine);
+  virtual ~TdmClient();
 
-  void AsyncWaitForVsync(intptr_t baton);
+  bool IsValid();
+  void AwaitVblank(intptr_t baton);
 
  private:
   static void VblankCallback(tdm_client_vblank* vblank,
@@ -37,6 +41,23 @@ class TizenVsyncWaiter {
   tdm_client_vblank* vblank_ = nullptr;
 
   intptr_t baton_ = 0;
+};
+
+class TizenVsyncWaiter {
+ public:
+  TizenVsyncWaiter(FlutterTizenEngine* engine);
+  virtual ~TizenVsyncWaiter();
+
+  void AsyncWaitForVsync(intptr_t baton);
+
+ private:
+  void SendMessage(int event, intptr_t baton);
+
+  static void RunVblankLoop(void* data, Ecore_Thread* thread);
+
+  std::shared_ptr<TdmClient> tdm_client_;
+  Ecore_Thread* vblank_thread_ = nullptr;
+  Eina_Thread_Queue* vblank_thread_queue_ = nullptr;
 };
 
 }  // namespace flutter
